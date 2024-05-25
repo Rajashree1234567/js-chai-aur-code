74090f8c37dada89b22c7fdd99d2d17ca3af9532
rajashree biswas <53520193+Rajashree1234567@users.noreply.github.com>
2024-05-25 12:43:53 +0000

    studymaterial

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,23 @@
+
+const accountId = 144553
+let accountEmail = 'raj@gmail.com'
+var accountPassword = '12345'
+accountCity = "jaipur"
+let accountstate;
+
+// accountId = 144
+accountEmail = 'biswas@gmail.com'
+accountPassword = '8888'
+accountCity = "bbsr"
+
+// const accountId = 999999
+// let accountEmail = 'satya@gmail.com'
+var accountPassword = '00000'
+accountCity = "kolata"
+
+// console.log(accountId, 'accountId');
+// console.log('accountEmail', accountEmail);
+// console.log('accountPassword', accountPassword);
+// console.log('accountCity', accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as newer version
+
+// alert("hello", 3+3) //we are using nodejs not browser
+
+console.log(3+3);
+
+let myname = 'rajashree'
+
+//number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+// undefined => value is not assigned
+//symbol => unique nature
+
+//object
+
+console.log(typeof myname);
+console.log(typeof '7777');
+console.log(typeof 7777);
+console.log(typeof 'null');
+console.log(typeof null);
+console.log(typeof undefined);
+
