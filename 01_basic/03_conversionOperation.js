6f10eb5111ac283fd66e25b84a7f1a486ce328de
rajashree biswas <53520193+Rajashree1234567@users.noreply.github.com>
2024-05-25 15:00:32 +0000

    conversiondata type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,20 @@
+
+let score = 99
+let scroreString = '88'
+let name = 'rajashree'
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof (scroreString));
+
+let valueNumber = Number(scroreString);
+console.log(typeof (valueNumber));
+console.log(valueNumber);
+
+console.log('null', null);
+console.log('boolean', false);
+console.log('undefined', undefined);
+console.log('zero', 0);
+
+let valueName = Number(name);
+console.log('valueName', valueName);
