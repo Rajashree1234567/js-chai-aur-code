e71cbd55ce528110f157a1bc3a2b2b2bb4cb5cac
rajashree biswas <53520193+Rajashree1234567@users.noreply.github.com>
2024-05-26 03:19:20 +0000

    maths

--- 01_basic/07_math.js
+++ 01_basic/07_math.js
@@ -10,3 +10,15 @@ console.log(Math.min(4, 6, 7, 5, 1));
 console.log(Math.max(4, 6, 7, 5, 1));
 
 console.log(Math.random()); //give value always 0 to 1
+console.log((Math.random()) * 10);
+console.log(Math.floor((Math.random()) * 10));
+
+console.log((Math.random() * 10) + 1);
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.random()  * (max - min +1));
+console.log(Math.floor(Math.random()  * (max - min +1)));
+console.log(Math.floor(Math.random()  * (max - min +1)) + min);
